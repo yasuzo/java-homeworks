6c7b6a9e59a82a68a0950205434b6a5cec4e7b86
yasuzo <jan.capek0@gmail.com>
2019-07-05 06:07:41 +0200

    hw17: Added javadoc to the application entry point.

--- hw17-0036509045-2/src/main/java/hr/fer/zemris/java/hw17/jvdraw/JVDraw.java
+++ hw17-0036509045-2/src/main/java/hr/fer/zemris/java/hw17/jvdraw/JVDraw.java
@@ -1,14 +1,12 @@
 package hr.fer.zemris.java.hw17.jvdraw;
 
-import hr.fer.zemris.java.hw17.jvdraw.color.ColorChangeListener;
-import hr.fer.zemris.java.hw17.jvdraw.color.IColorProvider;
-import hr.fer.zemris.java.hw17.jvdraw.tools.CircleTool;
-import hr.fer.zemris.java.hw17.jvdraw.tools.Tool;
-
 import javax.swing.*;
-import java.awt.*;
-import java.util.function.Supplier;
 
+/**
+ * Starting point for JVDraw paint application.
+ *
+ * @author Jan Capek
+ */
 public class JVDraw {
     public static void main(String[] args) {
 
